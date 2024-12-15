a2a87cd91ba45434e6f66714dc2a265890f3e048
Vincent Horecký <vincent.horecky@seznam.cz>
2024-12-15 17:41:56 +0100

    added types and intefaces to JS

--- conventions.go
+++ conventions.go
@@ -9,16 +9,16 @@ import (
 func javaScriptConventions(json JSONData, conceptIndex int) {
 	conceptHighlight := color.New(color.FgHiYellow).SprintFunc()
 	conventionHighlight := color.New(color.FgHiRed).SprintFunc()
-	keyword := color.New(color.FgHiYellow).SprintFunc()
-	specialOperator := color.New(color.FgHiYellow).SprintFunc()
+	keyword := color.New(color.FgRed).SprintFunc()
+	specialOperator := color.New(color.FgMagenta).SprintFunc()
 	name := color.New(color.FgHiBlue).SprintFunc()
-	number := color.New(color.FgMagenta).SprintFunc()
+	number := color.New(color.FgHiMagenta).SprintFunc()
 	function := color.New(color.FgHiCyan).SprintFunc()
 	bracket := color.New(color.FgBlue).SprintFunc()
 	operator := color.New(color.FgCyan).SprintFunc()
 	specialKeyword := color.New(color.FgHiRed).SprintFunc()
 	stringColor := color.New(color.FgGreen).SprintFunc()
-	class := color.New(color.FgHiMagenta).SprintFunc()
+	specialName := color.New(color.FgYellow).SprintFunc()
 
 	concept := json.Concepts["js"][conceptIndex-1]
 	convention := json.JavaScript[concept]
@@ -33,11 +33,19 @@ func javaScriptConventions(json JSONData, conceptIndex int) {
 		fmt.Printf("  %s %s%s\n", specialKeyword("return"), name("numberOfEmployes"), operator("++"))
 		fmt.Printf("%s\n", bracket("}"))
 	case "class":
-		fmt.Printf("%s %s %s\n", keyword("class"), class("SeniorEngineer"), bracket("{"))
+		fmt.Printf("%s %s %s\n", keyword("class"), specialName("SeniorEngineer"), bracket("{"))
 		fmt.Printf("  %s%s%s%s\n", specialKeyword("constructor"), bracket("("), name("name"), bracket(") {"))
 		fmt.Printf("    %s.%s = %s\n", keyword("this"), name("name"), name("name"))
 		fmt.Printf("    %s.%s = %s\n", keyword("this"), name("level"), number("6"))
 		fmt.Printf("  %s\n", bracket("}"))
+		fmt.Printf("%s\n", bracket("}"))
+	case "type":
+		fmt.Printf("%s %s = %s %s %s\n", keyword("type"), specialName("EngineersSeniority"), stringColor("Junior"), specialOperator("|"), stringColor("Senior"))
+	case "interface":
+		fmt.Printf("%s %s %s\n", keyword("interface"), specialName("ColorRgb"), bracket("{"))
+		fmt.Printf("  %s%s %s\n", name("red"), operator(":"), specialName("number"))
+		fmt.Printf("  %s%s %s\n", name("green"), operator(":"), specialName("number"))
+		fmt.Printf("  %s%s %s\n", name("blue"), operator(":"), specialName("number"))
 		fmt.Printf("%s\n", bracket("}"))
 	}
 }
